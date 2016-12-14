385d16e4a2a06d45569e83880962d71adfd6290a
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2016-12-14 11:23:32 +0900

    依存更新

--- app/src/main/java/jp/realglobe/sugo/actor/android/call/MainActivity.java
+++ app/src/main/java/jp/realglobe/sugo/actor/android/call/MainActivity.java
@@ -298,7 +298,7 @@ public class MainActivity extends AppCompatActivity {
         final String server = preferences.getString(getString(R.string.key_server), getString(R.string.default_server));
         final String key = getString(R.string.actor_prefix) + preferences.getString(getString(R.string.key_actor_suffix), getString(R.string.default_actor_suffix));
 
-        this.actor = new Actor(server, key, getString(R.string.name), null);
+        this.actor = new Actor(key, getString(R.string.name), null);
         final Emitter emitter;
         try {
             emitter = this.actor.addModule(getString(R.string.name), getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName, getString(R.string.description), new Object());
@@ -306,7 +306,7 @@ public class MainActivity extends AppCompatActivity {
             throw new RuntimeException();
         }
         this.actor.setOnConnect(() -> reportRoutine(emitter, nextReportId()));
-        this.actor.connect();
+        this.actor.connect(server);
     }
 
     private synchronized int nextReportId() {
